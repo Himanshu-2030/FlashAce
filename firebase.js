f107067127dc5c4a767cdfe059346e6d6bf155f5
Isha <142431248+IshaNair09@users.noreply.github.com>
2024-08-22 02:15:51 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import {getFirebase} from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAOBp_y5IKXOAb26AJUWEi6TQV4d5LTdJA",
-  authDomain: "flashai-d91bc.firebaseapp.com",
-  projectId: "flashai-d91bc",
-  storageBucket: "flashai-d91bc.appspot.com",
-  messagingSenderId: "486805290628",
-  appId: "1:486805290628:web:7f868a552b086e607b0cd1",
-  measurementId: "G-YHJM4Z6494"
+  apiKey:"",
+  authDomain:"", 
+  projectId:"",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId:"",
+  measurementId: "",
 };
 
 // Initialize Firebase
